4ca41559d24184cbe3e4094a25fb016da83e219f
ayushtenguria <hacktiger.as@gmail.com>
2023-06-08 11:15:29 +0530

    footer and navabr added

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import './App.css';
 import { AboutMe } from './Components/About Me/AboutMe';
 import { CardSection } from './Components/Card/CardSection';
 import { Contact } from './Components/Contact Me/ContactMe';
+import Footer from './Components/Footer/Footer';
 import { Header } from './Components/Header/Header';
+import { Navbar } from './Components/Header/Navbar/Navbar';
 import { WorkExperience } from './Components/Work/Work';
 import Flashlight from './Components/testCode';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Header/>
       <AboutMe/>
       <WorkExperience/>
       <Contact/>
       <CardSection/> 
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <div className="flex justify-center items-center z-10 static mt-9 ">
+      <nav className="bg-zinc-300/60 h-14 w-[70rem] rounded-3xl text-white p-4 z-10 fixed ">
+        This is Navbar
+      </nav>
+    </div>
+  );
+};
